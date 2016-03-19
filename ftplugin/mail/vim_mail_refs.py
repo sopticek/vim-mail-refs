73166ed830549e04ea53a27b7ecbe914ceb3e2f7
Daniela Duricekova <daniela.duricekova@gmail.com>
2016-03-19 09:35:11 +0100

    Add basic functionality to add_ref().

--- ftplugin/mail/vim_mail_refs.py
+++ ftplugin/mail/vim_mail_refs.py
@@ -3,5 +3,50 @@
 # Date:     2016-03-18
 #
 
+import re
+
+
 def add_ref(buffer, window, ref_url):
-    ...
+    row, col = _get_cursor_pos(window)
+    ref = _append_ref_url(buffer, ref_url)
+    row, col = _insert_ref(buffer, row, col, ref)
+    _set_cursor_pos(window, row, col)
+
+
+def _insert_ref(buffer, row, col, ref):
+    current_line = buffer[row]
+    before_pos = current_line[:col + 1]
+    after_pos = current_line[col + 1:]
+    new_line = before_pos + ref + after_pos
+    buffer[row] = new_line
+    if col == 0:
+        col = -1
+    return row, col + len(ref)
+
+
+def _append_ref_url(buffer, ref_url):
+    refs = _get_refs_with_urls(buffer)
+    ref = '[{}]'.format(len(refs) + 1)
+    if not refs:
+        buffer.append('')
+    buffer.append('{} {}'.format(ref, ref_url))
+    return ref
+
+
+def _get_refs_with_urls(buffer):
+    refs = []
+    for line in reversed(buffer):
+        m = re.match(r'(\[\d+\]) (.+)', line)
+        if m is None:
+            break
+        refs.append(m.groups())
+    return refs
+
+
+def _get_cursor_pos(window):
+    row, col = window.cursor
+    return row - 1, col
+
+
+def _set_cursor_pos(window, row, col):
+    window.cursor = (row + 1, col)
--- ftplugin/mail/vim_mail_refs_tests.py
+++ ftplugin/mail/vim_mail_refs_tests.py
@@ -1,5 +1,91 @@
 import unittest
+from unittest import mock
+
+from vim_mail_refs import add_ref
 
 
 class AddRefTests(unittest.TestCase):
-    ...
+    def setUp(self):
+        self.window = mock.Mock()
+
+    def test_ref_is_added_correctly_when_buffer_is_empty(self):
+        buffer = ['']
+        self.window.cursor = (1, 0)
+        ref_url = 'URL'
+
+        add_ref(buffer, self.window, ref_url)
+
+        self.assertEqual(
+            buffer,
+            [
+                '[1]',
+                #  ^
+                '',
+                '[1] URL'
+            ]
+        )
+        self.assertEqual(self.window.cursor, (1, 2))
+
+    def test_ref_is_appended_correctly_when_buffer_is_not_empty(self):
+        buffer = ['look at ']
+        #                 ^
+        self.window.cursor = (1, 7)
+        ref_url = 'URL'
+
+        add_ref(buffer, self.window, ref_url)
+
+        self.assertEqual(
+            buffer,
+            [
+                'look at [1]',
+                #          ^
+                '',
+                '[1] URL'
+            ]
+        )
+        self.assertEqual(self.window.cursor, (1, 10))
+
+    def test_ref_is_inserted_correctly_when_buffer_is_not_empty(self):
+        buffer = ['look at .']
+        #                 ^
+        self.window.cursor = (1, 7)
+        ref_url = 'URL'
+
+        add_ref(buffer, self.window, ref_url)
+
+        self.assertEqual(
+            buffer,
+            [
+                'look at [1].',
+                #          ^
+                '',
+                '[1] URL'
+            ]
+        )
+        self.assertEqual(self.window.cursor, (1, 10))
+
+    def test_second_ref_is_added_correctly(self):
+        buffer = [
+            'look at [1].',
+            'Also look at .',
+            #            ^
+            '',
+            '[1] URL1'
+        ]
+        self.window.cursor = (2, 12)
+        ref_url = 'URL2'
+
+        add_ref(buffer, self.window, ref_url)
+
+        self.assertEqual(
+            buffer,
+            [
+                'look at [1].',
+                'Also look at [2].',
+                #               ^
+                '',
+                '[1] URL1',
+                '[2] URL2'
+            ]
+        )
+        self.assertEqual(self.window.cursor, (2, 15))
